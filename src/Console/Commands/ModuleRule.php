e971086516ad576d4347c6714210b92e5cfd98f6
dot-env-it <dotenvit@gmail.com>
2026-04-28 11:32:20 +0530

    Added Rule

---
+++ src/Console/Commands/ModuleRule.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace DotEnvIt\ModuleBoilerplate\Console\Commands;
+
+class ModuleRule extends BaseModuleGenerator
+{
+    protected $name = 'module:rule';
+
+    protected $type = 'Rule';
+}
